4f095bfe2b7aa4d4e6e48049f4ca3ace17bf3e86
ShavaShav <zachshaver@gmail.com>
2018-04-01 23:43:53 -0400

    Add Users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's clear the users table first
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Use same hash for all users, for speed.
+        $password = Hash::make('toptal');
+
+        // Create "admin"
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+        // Create some regular users
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
