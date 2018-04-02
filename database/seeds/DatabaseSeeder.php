0701f803c7dac0b8d3e4ae577f6a1862825906f7
ShavaShav <zachshaver@gmail.com>
2018-04-01 23:44:20 -0400

    Combine seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(EventsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
         Model::reguard();
     }
